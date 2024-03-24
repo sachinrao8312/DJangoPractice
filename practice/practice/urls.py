0dea1e38cd5523cc9b4b33bb60df5c4ef413e598
Sachin Rao <biz.sachinrao@gmail.com>
2024-03-24 20:27:11 +0530

    Feat : isitIndependece app done learnt django-templating

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', include('hello.urls'))
-]
+    path('hello/', include('hello.urls')),
+    path('isitIndepenceDay/', include('isitIndepenceDay.urls'))
+    ]
+
